4246c012af27a3d0c0d8e30b68046a36f662e830
andrew wong <awwong92@gmail.com>
2018-05-09 19:56:16 -0400

    ADD: added new models for custom game features

---
+++ server/db/models/MapItem.js
@@ -0,0 +1,11 @@
+const Sequelize = require('sequelize');
+const db = require('../db')
+
+const MapItem = db.define('mapItem', {
+  location: {
+    type: Sequelize.STRING
+  }
+});
+
+module.exports = MapItem;
+
--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,5 +1,15 @@
 const Result = require('./Result');
+const CustomMap = require('./CustomMap');
+const Item = require('./Item');
+const MapItem = require('./MapItem');
+
+//Associations
+CustomMap.belongsToMany(Item, { through: MapItem});
+Item.belongsToMany(CustomMap, { through: MapItem});
 
 module.exports = {
-  Result
+  Result,
+  CustomMap,
+  Item,
+  MapItem
 };
