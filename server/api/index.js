2e90ec4018884ad74c24d8446fedda5e2a16e6c5
andrew wong <awwong92@gmail.com>
2018-05-09 20:40:15 -0400

    EDIT: updated index.js for api routes

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 module.exports = router;
 
+router.use('/users', require('./users'))
 router.use('/results', require('./results'))
 
 router.use((req, res, next) => {
