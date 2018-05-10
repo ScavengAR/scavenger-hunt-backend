8d4d96af5e1075922aaa62f63470bba2c5f5c712
andrew wong <awwong92@gmail.com>
2018-05-10 18:21:50 -0400

    ADD: added custom map route to user api

--- server/api/users.js
+++ server/api/users.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Result } = require('../db/models');
+const { User, Result, CustomMap } = require('../db/models');
 module.exports = router
 
 // GET /api/users
@@ -27,6 +27,23 @@ router.get('/:id/results', (req, res, next) => {
       res.status(404).send('No results found')
     } else {
       res.json(foundResults)
+    }
+  })
+  .catch(next)
+})
+
+// GET /api/users/:id/customMaps
+router.get('/:id/customMap', (req, res, next) => {
+  CustomMap.findAll({
+    where: {
+      userId: req.params.id
+    }
+  })
+  .then(foundCustomMaps => {
+    if (!foundCustomMaps) {
+      res.status(404).send('No custom maps found')
+    } else {
+      res.json(foundCustomMaps)
     }
   })
   .catch(next)
