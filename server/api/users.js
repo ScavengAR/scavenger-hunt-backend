81aef894125da709f51f647a304fbd13d5b95447
Marshall <27579685+dmzobel@users.noreply.github.com>
2018-05-11 10:53:20 -0400

    Update users.js
    Removed the GET for all users, was not secure

--- server/api/users.js
+++ server/api/users.js
@@ -2,19 +2,6 @@ const router = require('express').Router();
 const { User, Result, CustomMap } = require('../db/models');
 module.exports = router;
 
-// GET /api/users
-router.get('/', (req, res, next) => {
-  if (req.user !== undefined){
-    User.findAll({
-      attributes: ['id', 'email']
-    })
-    .then(users => res.json(users))
-    .catch(next)
-  } else {
-    res.status(404).send('Admin privileges are required to view this information.')
-  }
-});
-
 // GET /api/users/:id/results
 router.get('/:id/results', (req, res, next) => {
   Result.findAll({
