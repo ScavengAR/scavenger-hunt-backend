27d46aecbe3d668540d6bcad6a9cb08123b669b5
andrew wong <awwong92@gmail.com>
2018-05-10 20:44:54 -0400

    UPDATE: add more detail and validators to item model

--- server/db/models/Item.js
+++ server/db/models/Item.js
@@ -2,8 +2,25 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 
 const Item = db.define('item', {
-  imageData: {
-    type: Sequelize.STRING
+  z: { //z position - depth
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  x: { //x position - left and right
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  y: { //y position - up and down
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  speed: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  captured: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false
   }
 });
 
