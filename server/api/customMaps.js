704e7b0a97d128301f04a6ee06884e88e36680b8
andrew wong <awwong92@gmail.com>
2018-05-10 21:03:51 -0400

    ADD: add route to get items of a custom map

--- server/api/customMaps.js
+++ server/api/customMaps.js
@@ -1,10 +1,26 @@
 const router = require('express').Router();
-const { CustomMap } = require('../db/models');
+const { CustomMap, MapItem } = require('../db/models');
 module.exports = router;
 
 // GET /api/customMaps
 router.get('/', (req, res, next) => {
   CustomMap.findAll()
   .then(customMaps => res.json(customMaps))
+  .catch(next);
+})
+
+// GET/api/customMaps/:id
+router.get('/:id', (req, res, next) => {
+  MapItem.findOne({
+    where: {
+      customMapId: req.params.id
+    },
+    include: [{
+      all: true
+    }]
+  })
+  .then(itemsForMap => {
+    res.json(itemsForMap)
+  })
   .catch(next);
 })
