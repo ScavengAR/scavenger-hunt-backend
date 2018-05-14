d95d5ac7d8cc9971118c8d1fb4d0d313daf9ee5c
Val Pilossof <valpil610@gmail.com>
2018-05-13 20:56:05 -0400

    FIXED: request methods in auth

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,22 +1,24 @@
-const router = require('express').Router()
-const User = require('../db/models/user')
-module.exports = router
+const router = require('express').Router();
+const User = require('../db/models/user');
+module.exports = router;
 
-router.post('/login', (req, res, next) => {
-  User.findOne({where: {email: req.body.email}})
+router.put('/login', (req, res, next) => {
+  User.findOne({ where: { email: req.body.email } })
     .then(user => {
+      console.log('user', user);
       if (!user) {
-        console.log('No such user found:', req.body.email)
-        res.status(401).send('Wrong username and/or password')
+        console.log('No such user found:', req.body.email);
+        res.status(401).send('Wrong username and/or password');
       } else if (!user.correctPassword(req.body.password)) {
-        console.log('Incorrect password for user:', req.body.email)
-        res.status(401).send('Wrong username and/or password')
+        console.log('Incorrect password for user:', req.body.email);
+        res.status(401).send('Wrong username and/or password');
       } else {
-        req.login(user, err => (err ? next(err) : res.json(user)))
+        console.log('in the else');
+        req.login(user, err => (err ? next(err) : res.json(user)));
       }
     })
-    .catch(next)
-})
+    .catch(next);
+});
 
 router.post('/signup', (req, res, next) => {
   User.create({
@@ -24,25 +26,25 @@ router.post('/signup', (req, res, next) => {
     password: req.body.password
   })
     .then(user => {
-      req.login(user, err => (err ? next(err) : res.json(user)))
+      req.login(user, err => (err ? next(err) : res.json(user)));
     })
     .catch(err => {
       if (err.name === 'SequelizeUniqueConstraintError') {
-        res.status(401).send('User already exists')
+        res.status(401).send('User already exists');
       } else {
-        next(err)
+        next(err);
       }
-    })
-})
+    });
+});
 
-router.post('/logout', (req, res, next) => {
-    req.logout()
-    req.session.destroy()
-    res.redirect('/')
-})
+router.delete('/logout', (req, res, next) => {
+  req.logout();
+  req.session.destroy();
+  res.redirect('/');
+});
 
 router.get('/me', (req, res) => {
-  res.json(req.user)
-})
+  res.json(req.user);
+});
 
-router.use('/google', require('./google'))
+router.use('/google', require('./google'));
