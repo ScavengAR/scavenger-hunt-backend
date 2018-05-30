18770e86f4465c2b3ee4dea15abb5f4c63dd6c27
Marshall Zobel <dmzobel@gmail.com>
2018-05-30 12:19:17 -0400

    ADD: table association and api route for unique leaderboards

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -2,12 +2,20 @@ const User = require('./User')
 const Result = require('./Result');
 const CustomMap = require('./CustomMap');
 
-//Associations
+/*
+ * Associations
+ */
+
 User.hasMany(Result);
 Result.belongsTo(User, {as: 'user'});
 
+// Allows users to access the custom maps they create
 User.hasMany(CustomMap);
 CustomMap.belongsTo(User);
+
+// Ensures that a unique leaderboard will be maintained for each custom map
+CustomMap.hasMany(Result);
+Result.belongsTo(CustomMap);
 
 module.exports = {
   User,
